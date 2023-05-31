d868f1369021e28d9fe276119f14ab1d79b374bd
Saksham Sangal <sakshamsangal111@gmail.com>
2023-05-31 12:04:19 +0530

    up

--- code/dsa/Graph.java
+++ code/dsa/Graph.java
@@ -1,11 +1,27 @@
 package com.example.dsa;
+
 import java.util.*;
+
 public class Graph {
-    int size;
-    private List<List<Integer>> adjacencyList;
-    public void addEdge(Integer vertex1, Integer vertex2) {
-        adjacencyList.get(vertex1).add(vertex2);
+    // No. of vertices
+    private int V;
+
+    // Adjacency Lists
+    private LinkedList<Integer> adj[];
+
+    // Constructor
+    Graph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
     }
+
+    // Function to add an edge into the graph
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
     // BFS traversal
     // connected graph
     public void bfs() {
@@ -24,6 +40,7 @@ public class Graph {
             }
         }
     }
+
     // connected graph dfs
     public void dfs(int vertex, boolean[] visited) {
         System.out.print(vertex + " ");
@@ -33,5 +50,20 @@ public class Graph {
                 dfs(adjacent, visited);
             }
         }
+    }
+
+    // Driver code
+    public static void main(String args[]) {
+        Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Following is Breadth First Traversal " + "(starting from vertex 2)");
+
+        g.BFS(2);
     }
 }
